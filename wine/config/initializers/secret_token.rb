eb8f32f3904bb9f8cebdcbd55b0451bb3e520275
Amanda Garfinkel <agarfinkel2013@kellogg.northwestern.edu>
2013-05-18 16:36:59 -0500

    second commit

---
+++ wine/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wine::Application.config.secret_token = '6982c5c08bbd5af11475bb6499eb2d9c70dc026fa952706984cf1f9a93c3ef2464a645c5b9bae611babeab952f73de2ea077e6fc5aa025f2b6ca3fe4b288c48d'
